30563dd02059548e7d0e0f53045c87a426fdfa43
Jennyads <jennycristina.1998@gmail.com>
2023-06-16 16:56:11 -0300

    Adds connection to Postgres Bank

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/seu-usuario/crud_product_go
+
+go 1.20
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
+	"database/sql"
 	"html/template"
 	"net/http"
 )
 
+func conectaComBancoDeDados() *sql.DB{
+	conexao := "user=postgres dbname=loja_alura password=postgres host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
 type Produto struct {
+	ID int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -20,12 +32,41 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
-		{"Tenis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
-		{"Produto novo", "Muito legal", 1.99, 1},
+	db := conectaComBancoDeDados()
+
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	if err != nil{
+		panic(err.Error())
+	}
+
+	//Cada produto do banco de dados vou chamar de p, assim que eu criar todos os meus produtos, 
+	//eu vou inserir uma variável chamada produtos a nossa lista, nosso slice, passo ele embaixo.
+	p := Produto{}
+	produtos := []Produto{}
+
+	//o for, linha a linha vai vendo o que cada produto é, for do select de produtos e usar uma função chamada next. Ou seja, eu quero a próximo à linha – leu a primeira eu quero a próxima linha, vou abrir parênteses, abro e fecho chaves também.
+	for selectDeTodosOsProdutos.Next(){
+		var id, quantidade int 
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+		    
 	}
 
 	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
+
 }
+
+	
