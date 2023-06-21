cf1748212f1604cb7e352cb61509ba18b1580ec5
Jennyads <jennycristina.1998@gmail.com>
2023-06-21 16:58:09 -0300

    add insert function

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -15,3 +15,30 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
 
 }
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST"{
+		nome := r.FormValue("nome")
+		descricao:= r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+
+		quantidadeConvertidaParaInt, err := strconv.Atoi(preco)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade:", err)
+		}
+
+		models.CriaNovoProduto(nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+	}
+	http.Redirect(w, r, "/", 301)
+
+}
