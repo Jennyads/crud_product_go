fa61ba4c61f71b87374d37dd23ed16bb251d9804
Jennyads <jennycristina.1998@gmail.com>
2023-06-29 21:51:54 -0300

    add delete product function

--- models/produtos.go
+++ models/produtos.go
@@ -3,7 +3,7 @@ package models
 import "github.com/seu-usuario/crud_product_go/db"
 
 type Produto struct {
-	Id         int
+	Id        int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -52,6 +52,17 @@ func CriaNovoProduto(nome, descricao string, preco float64, quantidade int) {
 	}
 
 	insereDadosNoBanco.Exec(nome, descricao, preco, quantidade)
+	defer db.Close()
+
+}
+
+func DeletaProduto(id string){
+	db := db.ConectaComBancoDeDados()
+	deletarOProduto, err := db.Prepare("delete from produtos where id=$1")
+	if err != nil {
+		panic(err.Error())
+	}
+	deletarOProduto.Exec(id)
 	defer db.Close()
 
 }
